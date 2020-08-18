1fc3f5f9cbbd22c8d5506b3e34a9f59e9bac4568
Anatoliy Yakimov <yakimov.tolya@gmail.com>
2020-08-18 18:42:06 +0300

    Fixed some issues with configuration and database bootstrapping

--- database/src/main/docker/bootstrap_db.sql
+++ database/src/main/docker/bootstrap_db.sql
@@ -1,27 +1,30 @@
+CREATE TABLE license
+(
+    license_id        VARCHAR(100) PRIMARY KEY NOT NULL,
+    organization_id   TEXT                     NOT NULL,
+    license_type      TEXT                     NOT NULL,
+    product_name      TEXT                     NOT NULL,
+    license_max       INT                      NOT NULL,
+    license_allocated INT,
+    comment           VARCHAR(1000)
+);
 
-CREATE TABLE license (
-                          license_id        VARCHAR(100) PRIMARY KEY NOT NULL,
-                          organization_id   TEXT NOT NULL,
-                          license_type      TEXT NOT NULL,
-                          product_name      TEXT NOT NULL,
-                          license_max       INT   NOT NULL,
-                          license_allocated INT,
-                          comment           VARCHAR(1000));
-
-COPY license(license_id, organization_id, license_type, product_name, license_max, license_allocated, comment)
+COPY license (organization_id, license_id, product_name, license_type, license_max, license_allocated, comment)
     FROM '/license_data.csv'
     DELIMITER ','
     CSV HEADER;
 
 
-CREATE TABLE organization (
-                               organization_id        VARCHAR(100) PRIMARY KEY NOT NULL,
-                               name                   TEXT NOT NULL,
-                               contact_name           TEXT NOT NULL,
-                               contact_email          TEXT NOT NULL,
-                               contact_phone          TEXT   NOT NULL);
+CREATE TABLE organization
+(
+    organization_id VARCHAR(100) PRIMARY KEY NOT NULL,
+    name            TEXT                     NOT NULL,
+    contact_name    TEXT                     NOT NULL,
+    contact_email   TEXT                     NOT NULL,
+    contact_phone   TEXT                     NOT NULL
+);
 
-COPY organization(organization_id, contact_email, contact_name, contact_phone, name)
+COPY organization (organization_id, contact_email, contact_name, contact_phone, name)
     FROM '/organization_data.csv'
     DELIMITER ','
     CSV HEADER;
